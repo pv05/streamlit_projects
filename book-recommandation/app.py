72a1fea350897e45cb440184fbecaac340f7c2ac
pankaj verma <60501879+pv05@users.noreply.github.com>
2022-03-02 21:30:40 +0530

    link change and some rqrmnt chnages

--- book-recommandation/app.py
+++ book-recommandation/app.py
@@ -38,32 +38,32 @@ def recommand_book1(book_name):
 
 
 if st.button('Recommand'):
-    st.write('Recommanded Suggestions')
+    st.header('Recommanded Suggestions')
     book,link = recommand_book1(selected_book)
 
     col1,col2,col3,col4,col5 = st.columns(5)
 
     with col1:
         link[0] = link[0].replace('http://images.amazon.com/','https://m.media-amazon.com/')
-        link[0] = link[0].replace('https://dummyimage.com/200x200/000000/fff.png&text=No+Image+Preview','https://dummyimage.com/305x500/000000/fff.png&text=No+Image+Preview')
+        link[0] = link[0].replace('https://dummyimage.com/200x200/000000/fff.png&text=No+Image+Preview','https://dummyimage.com/305x500/000000/fff.png&text=Dummy+Image+Preview')
 
         st.text(book[0])
         st.image(link[0])
     with col2:
         link[1] = link[1].replace('http://images.amazon.com/','https://m.media-amazon.com/')
-        link[1] = link[1].replace('https://dummyimage.com/200x200/000000/fff.png&text=No+Image+Preview','https://dummyimage.com/305x500/000000/fff.png&text=No+Image+Preview')
+        link[1] = link[1].replace('https://dummyimage.com/200x200/000000/fff.png&text=No+Image+Preview','https://dummyimage.com/305x500/000000/fff.png&text=Dummy+Image+Preview')
 
         st.text(book[1]) 
         st.image(link[1])
     with col3:
         link[2] = link[2].replace('http://images.amazon.com/','https://m.media-amazon.com/')
-        link[2] = link[2].replace('https://dummyimage.com/200x200/000000/fff.png&text=No+Image+Preview','https://dummyimage.com/305x500/000000/fff.png&text=No+Image+Preview')
+        link[2] = link[2].replace('https://dummyimage.com/200x200/000000/fff.png&text=No+Image+Preview','https://dummyimage.com/305x500/000000/fff.png&text=Dummy+Image+Preview')
 
         st.text(book[2]) 
         st.image(link[2])
     with col4:
         link[3] = link[3].replace('http://images.amazon.com/','https://m.media-amazon.com/')
-        link[3] = link[3].replace('https://dummyimage.com/200x200/000000/fff.png&text=No+Image+Preview','https://dummyimage.com/305x500/000000/fff.png&text=No+Image+Preview')
+        link[3] = link[3].replace('https://dummyimage.com/200x200/000000/fff.png&text=No+Image+Preview','https://dummyimage.com/305x500/000000/fff.png&text=Dummy+Image+Preview')
 
         st.text(book[3]) 
         st.image(link[3])
